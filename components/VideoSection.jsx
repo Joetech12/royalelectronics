fb0a7e1bc1cd7a371c53b987dd9ce56c1cb67e75
Ifeanyi Umeh <ifeanyi4umeh@gmail.com>
2022-06-07 12:22:51 +0100

    Added video section

---
+++ components/VideoSection.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactPlayer from "react-player";
+
+const VideoSection = () => {
+  return (
+    <div className="w-full bg-royal">
+      <div className="flex justify-center py-[0px]">
+        {/* <video 
+        src="/sign.mp4" 
+        autoPlay={true} 
+        loop 
+        controls/> */}
+        <video
+          className=" bg-gray-900 bg-blend-multiply object-cover h-full w-full"
+          src="/sign.mp4"
+          autoPlay
+          loop
+          muted
+          controls
+        />
+      </div>
+    </div>
+  );
+};
+
+export default VideoSection;
