54a7e40ac13e2a9c64fd8ffc76466fd8531e2edf
Ifeanyi Umeh <ifeanyi4umeh@gmail.com>
2022-06-08 07:06:27 +0100

    changed most popular to best sellers

--- components/PopularSection.jsx
+++ components/PopularSection.jsx
@@ -1,23 +1,21 @@
-import React, { useState, useEffect, } from "react";
+import React, { useState, useEffect } from "react";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import HighlyRated from "./HighlyRated";
 import Latest from "./Latest";
 import MostPopular from "./MostPopular";
 import { useDispatch } from "react-redux";
-import { dropHandler, dropHandler2, dropHandler3 } from '../slices/dropSlice';
-import { selectValue, selectValue2 } from '../slices/dropSlice';
+import { dropHandler, dropHandler2, dropHandler3 } from "../slices/dropSlice";
+import { selectValue, selectValue2 } from "../slices/dropSlice";
 import { useSelector } from "react-redux";
 
 const PopularSection = () => {
-
-
   const dispatch = useDispatch();
 
   const dropValue = useSelector(selectValue);
 
   const dropValue2 = useSelector(selectValue2);
 
-  console.log()
+  console.log();
   // const [drop, setDrop] = useState(true);
   // const [drop2, setDrop2] = useState(false);
 
@@ -29,7 +27,6 @@ const PopularSection = () => {
     <div className="">
       <div className="bg-white text-white text-[13px] ">
         <div className="max-w-[1240px] mx-auto px-[20px] h-full flex justify-center items-center ">
-          
           <div className="flex flex-col items-center divide-y-[4px]">
             <div className="text-black xs:text-[18px] md:text-[24px] font-bold flex space-x-[0px] h-full pt-[20px]">
               {/* most popular */}
@@ -39,7 +36,7 @@ const PopularSection = () => {
               >
                 <div className="flex items-center px-[20px]">
                   <p className="cursor-pointer duration-300 group-hover:text-[#464A8A]">
-                    MOST POPULAR
+                    BEST SELLERS
                   </p>
                 </div>
               </button>
@@ -54,19 +51,14 @@ const PopularSection = () => {
                   </p>
                 </div>
               </button>
-              
-
             </div>
 
             <div className="text-black text-[20px] font-bold relative">
               <div className="">{dropValue ? <MostPopular /> : null}</div>
-              <div className="z-[20] absolute top-0">{dropValue2 ? <Latest /> : null}</div>
+              <div className="z-[20] absolute top-0">
+                {dropValue2 ? <Latest /> : null}
               </div>
-
-
-
-
-
+            </div>
           </div>
         </div>
       </div>
