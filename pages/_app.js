beb685585acf139eedeb0c55e31cae3c97aedfd1
Ifeanyi Umeh <ifeanyi4umeh@gmail.com>
2022-06-14 02:51:17 +0100

    Added store locator

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
+import "../styles/nprogress.css";
 import { store } from "../store";
 import { Provider } from "react-redux";
 import Footer from "../components/Footer";
+import Router from "next/router";
+import nProgress from "nprogress";
+
+
+Router.events.on("routeChangeStart", nProgress.start)
+Router.events.on("routeChangeError", nProgress.done)
+Router.events.on("routeChangeComplete", nProgress.done)
 
 function MyApp({ Component, pageProps }) {
   return (
